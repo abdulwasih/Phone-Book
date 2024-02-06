a1fd83715b1bc977e37baa10a9e0f60e2f75025f
abdulwasih <vasih95@gmail.com>
2024-02-06 12:01:05 +0530

    handled submit button

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [firstName,setFirstName] = useState('')
+  const [lastName,setLastName] = useState('')
+  const [phone,setPhone] = useState('')
+  //styling
+  const tableStyle =  {
+    margin: 'auto',
+    width: '50%',
+    padding: '10px'
+}
+  const handleSubmit=(event)=>{
+    event.preventDefault()
+    console.log(firstName,lastName,phone)
+  }
+
   return (
     <div className="App">
       <h1>Phone Book</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>First Name</label>
         <input
           type='text'
           name='firstName'
+          value={firstName}
+          onChange={(event)=>setFirstName(event.target.value)}
         >
         </input><br></br>
         <label>Last Name</label>
         <input
           type='text'
           name='lastName'
-        ></input><br></br>
+          value={lastName}
+          onChange={(event)=>setLastName(event.target.value)}
+x        ></input><br></br>
         <label>Phone</label>
         <input
-          type='text'
+          type='number'
           name='phone'
+          value={phone}
+          onChange={(event)=>setPhone(event.target.value)}
         >
         </input><br></br>
         <button type='submit'>Submit</button>
       </form>
-      
+      <table style={tableStyle}>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Phone</th>
+        </tr>
+      </table>
+        <tr>
+          
+        </tr>
     </div>
   );
 }
